694d1504687e3c512b05f249324fb52d86fd8468
2a2le <2a2le@example.com>
2016-05-20 16:30:52 +0300

    More

--- ch1/ch1.cpp
+++ ch1/ch1.cpp
@@ -1,13 +1,14 @@
 // Chapter 1 - Arrays and Strings
 
 #include<iostream>
+#include<cstdlib>
 
 // helpers
 
 // Mergesort
 // TODO implement mergesort for strings
-void merge(std::string str, int low, int middle, int high);
-void mergesort(std::string str, int low, int high) {
+void merge(char **str, int low, int middle, int high);
+void mergesort(char **str, int low, int high) {
     int middle; /* index of middle element */
 
     if (low < high) {
@@ -18,12 +19,36 @@ void mergesort(std::string str, int low, int high) {
     }
 }
 
-void merge(std::string str, int low, int middle, int high) {
-    //int i;                  /* counter */
-    //std::queue buffer1, buffer2; /* buffers to hold elements for merging */ 
+void merge(char **str, int low, int middle, int high) {
+    int i, j;                  /* counter */
+   
+    std::cout << "low: " << low << std::endl;  
+    std::cout << "middle: " << low << std::endl;  
+    std::cout << "high: " << low << std::endl;  
+    
+    char *buffer1 = (char*) malloc(50);
+    char *buffer2 = (char*) malloc(50); /* buffers to hold elements for merging */ 
+    for (j=0; j<middle-low+1; j++) buffer1[j] = *str[low+j];
+    buffer1[j] = 0;
+    for (j=0; j<high-middle; j++) buffer2[j] = *str[middle+j];
+    buffer2[j] = 0;
 
-    //init_queue(&buffer1);
-    //init_queue(&buffer2);
+    std::cout << buffer1 << std::endl;
+    std::cout << buffer2 << std::endl;
+    std::cout << *str << std::endl;
+
+    i = low;
+    while (*buffer1 && *buffer2) {
+        if (*buffer1 < *buffer2) {
+            (*str)[i++] = *(buffer1++);
+        } else {
+            (*str)[i++] = *(buffer2++);
+        }
+
+    }
+
+    while (*buffer1) (*str)[i++] = *(buffer1++);
+    while (*buffer2) (*str)[i++] = *(buffer2++);
 }
     
 
@@ -98,6 +123,23 @@ int main() {
     std::cout << "Strings |unu| and |uun|: " << is_permutation("unu", "uun") << std::endl;
     std::cout << "Strings |unu| and |una|: " << is_permutation("unu", "una") << std::endl;
     std::cout << "Strings |123456| and |352416|: " << is_permutation("123456", "352416") << std::endl;
-    std::cout << "Strings |123456| and |353416|: " << is_permutation("123456", "353416") << std::endl;
+
+    int i, n = 6;
+    const char *s ="126456";
+    char *str = (char*) malloc(100);
+    for (i=0;i<n;i++) {
+        str[i] = s[i];
+    }
+    str[i] = 0;
+    std::cout << std::endl << std::endl;
+    mergesort(&str, 0, 5);
+    std::cout << "mergesort |" << s << "|:" << str; 
+    /*const char s1 = "cdagfdfg";
+    std::cout << "mergesort |" << s1 << "|:"; 
+    mergesort(&s1, 0, 8);
+    std::cout << s1 << std::endl;*/
+    //std::cout << "Strings |123456| and |353416|: " << is_permutation("123456", "353416") << std::endl;
+    //std::cout << "Strings |123456| and |353416|: " << is_permutation("123456", "353416") << std::endl;
+    //std::cout << "Strings |123456| and |353416|: " << is_permutation("123456", "353416") << std::endl;
     std::cout << "______________________________________" << std::endl;
 }
